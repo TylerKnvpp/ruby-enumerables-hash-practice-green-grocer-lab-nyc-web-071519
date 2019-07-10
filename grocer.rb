ffc825b2575db96ce757ff13b92bb1e11aa505e6
tyler knapp <tgknapp11@gmail.com>
2019-07-10 12:45:34 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,22 @@
 def consolidate_cart(cart)
-  # code here
-end
+  my_cart = {}
 
-def apply_coupons(cart, coupons)
-  # code here
-end
+  cart.each do |hash|
+    keys = hash.keys
+      keys.each do |k|
+      v = hash[k]
+      my_cart_item_value = my_cart[k]
+        if my_cart_item_value.nil?
+          v[:count] = 1
+          my_cart[k] = v
+        else
+          v[:count] = my_cart_item_value[:count] + 1
+          my_cart[k] = v
+        end
+        puts "#{k}: #{my_cart[k]}"
+      end
+    end
+    my_cart
+  end
 
-def apply_clearance(cart)
-  # code here
-end
-
-def checkout(cart, coupons)
-  # code here
-end
+  
