95db1a2e340c6ef71ed3c8b8b1c8d670a2ea1a04
tyler knapp <tgknapp11@gmail.com>
2019-07-10 18:16:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -98,10 +98,11 @@ def checkout(cart, coupons)
         #total = the sum of the clearance_cart value :price mutliplied by the same arrays :count value
         total += clearance_cart[item][:price] * clearance_cart[item][:count]
     #Conditional: If the total amounts to over 100
-    # if total > 100
-    #   #then the total will be multiplied by 0.9(10%)
-    #   total = total * 0.9
+    if total > 100
+      #then the total will be multiplied by 0.9(10%)
+      total = total * 0.9
+    end
     end
   #return total
-  total > 100.00 ? (total * 0.90).round : total
+  total 
 end
