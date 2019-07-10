7c892c5fb940450de8f34737f63f2027651e6cf1
tyler knapp <tgknapp11@gmail.com>
2019-07-10 12:46:50 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,4 +19,25 @@ def consolidate_cart(cart)
     my_cart
   end
 
-  
+  def apply_coupons(cart, coupons)
+    coupons.each do |coupon|
+      if cart.keys.include? coupon[:item]
+        if cart[coupon[:item]][:count] >= coupon[:num]
+          new_name = "#{coupon[:item]} W/COUPON"
+          if cart[new_name]
+            cart[new_name][:count] += coupon[:num]
+          else
+            cart[new_name] = {
+              count: coupon[:num],
+              price: coupon[:cost]/coupon[:num],
+              clearance: cart[coupon[:item]][:clearance]
+            }
+          end
+          cart[coupon[:item]][:count] -= coupon[:num]
+        end
+      end
+    end
+    cart
+  end
+
+
