d74c75e7944736ac1cab6fc4a9045a948ec90674
tyler knapp <tgknapp11@gmail.com>
2019-07-10 16:44:07 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -90,12 +90,12 @@ def checkout(cart, coupons)
         cart_with_discounts_applied = apply_clearance(cart_with_coupons_applied)
 
         total = 0.0
-        
+
         cart_with_discounts_applied.keys.do |item|
       total += cart_with_discounts_applied[item][:price] * cart_with_discounts_applied[item][:count]
     end
     if total > 100
       total = total * 0.9
   end
-  total    
+  total
 end
