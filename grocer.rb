5d512a98178041e5909990839604ea4116730909
tyler knapp <tgknapp11@gmail.com>
2019-07-10 16:56:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -85,17 +85,23 @@ end
 
 
 def checkout(cart, coupons)
+  #created a variable - my_cart == to calling the consolidated_cart method on the cart array
     my_cart = consolidated_cart(cart)
+  #created the variable cart_with_coupons_applied set == the apply_coupons methods taking in my_cart and the coupons array
       cart_with_coupons_applied = apply_coupons(my_cart, coupons)
+    #created the variable cart_with_discounts_applied == the apply_clearance method taking in the cart_with_coupons_applied array
         cart_with_discounts_applied = apply_clearance(cart_with_coupons_applied)
-
+      #created a variable - total == 0.0
         total = 0.0
-
+        #iterating over the keys of the cart_with_discounts_applied array
         cart_with_discounts_applied.keys.do |item|
-      total += cart_with_discounts_applied[item][:price] * cart_with_discounts_applied[item][:count]
-    end
+        #total = the sum of the cart_with_discounts_applied value :price mutliplied by the same arrays :count value
+        total += cart_with_discounts_applied[item][:price] * cart_with_discounts_applied[item][:count]
+    #Conditional: If the total amounts to over 100
     if total > 100
+      #then the total will be multiplied by 0.9(10%)
       total = total * 0.9
   end
+  #return total
   total
 end
